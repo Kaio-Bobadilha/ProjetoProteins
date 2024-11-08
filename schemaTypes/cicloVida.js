09da2d9f2649d624a01d127d3e71831b13eff51c
Kaio Quevedo <kaiobobadilha@gmail.com>
2024-11-08 20:47:29 -0300

    Ajuste e arrumar
    Os codigos de Ciclo de vida e Vantagens

--- schemaTypes/cicloVida.js
+++ schemaTypes/cicloVida.js
@@ -1,19 +1,38 @@
 export default {
-    name: 'cicloVida',
-    type: 'document',
-    title: 'Ciclo de Vida',
-    fields: [
-      {
-        name: 'titulo',
-        type: 'string',
-        title: 'Título',
-      },
-      {
-        name: 'conteudo',
-        type: 'array',
-        of: [{ type: 'block' }],
-        title: 'Conteúdo',
-      },
-    ],
-  };
-  
+  name: 'cicloDeVida',
+  type: 'document',
+  title: 'Ciclo de Vida',
+  fields: [
+    {
+      name: 'titulo',
+      type: 'string',
+      title: 'Título',
+      description: 'Título da seção "Ciclo de Vida"'
+    },
+    {
+      name: 'estagios',
+      type: 'array',
+      title: 'Estágios',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {
+              name: 'nome',
+              type: 'string',
+              title: 'Nome do estágio'
+            },
+            {
+              name: 'imagem',
+              type: 'image',
+              title: 'Imagem do estágio',
+              options: {
+                hotspot: true
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
